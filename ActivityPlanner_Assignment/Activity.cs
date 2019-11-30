eac6f56a24a32e3c9228ceda087e8c9b50d00a8b
Hannah147 <47416196+Hannah147@users.noreply.github.com>
2019-11-30 18:07:34 +0000

    Created Activity Class and added Properties

---
+++ ActivityPlanner_Assignment/Activity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityPlanner_Assignment
+{
+    // Declare an enum in the namespace so it can be used throughout the class and methods
+    public enum ActivityType { Air, Water, Land}
+    class Activity
+    {
+        // Declare Properties
+        public string Name { get; set; }
+        public DateTime ActivityDate { get; set; }
+        public decimal Cost { get; set; }
+        private string _description;
+
+        public string Description
+        {
+            get
+            {
+                return $"{_description} Cost - {Cost:C}";
+            }
+            set
+            {
+                _description = value;
+            }
+        }
+
+        public static decimal TotalCost;
+        public ActivityType TypeOfActivity { get; set; }
+
+        // Constructors
+        public Activity(string name, DateTime activityDate, decimal)
+        public Activity():this("Unknown", DateTime.Now, 0, "Unknown", ActivityType.Air)
+        {
+
+        }
+
+
+    }
+}
